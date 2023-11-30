13dd7ddf066fd9b4ccf606a1652d25400a0612ea
Kernal Tux <150765007+Kernal-Tux@users.noreply.github.com>
2023-11-30 00:27:44 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "1234Cvbn@"
+
+@app.route("/hello")
+def index():
+	flash("what's your name?")
+	return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you! God Bless!")
+	request.form['name_input']
+	return render_template("index.html")
